4e5d9134ba68a5378ad25f4acac55f70d0e4717a
Hilary Azolibe <hilary.azolibe@gmail.com>
2017-10-08 12:23:53 +0100

    Refactor step definition to have conditions

--- src/test.e2e/step_definitions/punterFixtureValues.js
+++ src/test.e2e/step_definitions/punterFixtureValues.js
@@ -9,44 +9,8 @@
             var fixtureTwo = this.currentPage.fixtureTwo;
             var fixtureThree = this.currentPage.fixtureThree;
             var fixtureOneBest = this.currentPage.fixtureOneBest;
-            var fixtureOneBestHref = this.currentPage.fixtureOneBestHref;
             var fixtureTwoBest = this.currentPage.fixtureTwoBest;
             var fixtureThreeBest = this.currentPage.fixtureThreeBest;
-            // this.pageUtils.scrollToElement(fixtureOne);
-            // fixtureOne.getText().then(function (value) {
-            //     var fixture1 = value.toString();
-            //     return console.log('fixture one value is ' + fixture1)
-            // });
-            //
-            // fixtureTwo.getText().then(function (value) {
-            //     var fixture2 = value.toString();
-            //     return console.log('fixture two value is ' + fixture2)
-            // });
-            //
-            // fixtureThree.getText().then(function (value) {
-            //     var fixture3 = value.toString();
-            //     return console.log('fixture two value is ' + fixture3);
-            //     // if (fixture3 === 'Draw') {
-            //     //     console.log('fixture 3 has value Draw')
-            //     // }
-            // });
-            // fixtureOneBest.getText().then(function (value) {
-            //     var fixture1Best = value.toString();
-            //     return console.log('fixture one best value is ' + fixture1Best);
-            // });
-            // fixtureTwoBest.getText().then(function (value) {
-            //     var fixture2Best = value.toString();
-            //     return console.log('fixture Two best value is ' + fixture2Best);
-            // });
-            // fixtureThreeBest.getText().then(function (value) {
-            //     var fixture3Best = value.toString();
-            //     return console.log('fixture three best value is ' + fixture3Best);
-            // });
-            // var completeFixture;
-            // completeFixture.then(function () {
-            //
-            // });
-            // console.log("fixture = " + fixtureOne.getText().toString() + "" + fixtureTwo.getText().toString() + "" + fixtureThree.getText().toString());
 
             fixtureOne.getText().then(function (value) {
                 var fixture1 = value.toString();
@@ -58,16 +22,10 @@
                 return console.log('fixture two value is ' + fixture2);
             });
 
-
             fixtureThree.getText().then(function (value) {
                 var fixture3 = value.toString();
                 return console.log('fixture two value is ' + fixture3);
             });
-            // if (fixture3 === 'Draw') {
-            //     console.log('fixture 3 has value Draw')
-            // }
-
-            // console.log("fixture = " + fixtureOne.getText().toString() + "" + fixtureTwo.getText().toString() + "" + fixtureThree.getText().toString());
 
             fixtureOneBest.getText().then(function (value) {
                 var fixture1Best = value.toString();
@@ -82,60 +40,37 @@
                 var fixture3Best = value.toString();
                 return console.log('fixture three best value is ' + fixture3Best);
             });
-            // browser.element(:tag_name, 'html').attribute_value('lang')
-            // fixtureOneBestHref.
-
-
-            // var elementValue = browser.driver.executeScript('document.querySelectorAll("tr.oc-table__tr > td.best > a")[0].href');
-            // console.log('fixture onr best href ' + elementValue);
 
             function test1() {
                 getFixtureOneBestHref().then(function (item) {
                     console.log(item);
                     var str = item;
                     var res = str.substring(32, 36);
-                    switch (res) {
-                        case 0:
-                            res = "ubet";
-                            console.log('its ubet site');
-                            break;
-                        case 1:
-                            res = "luxb";
-                            console.log('its luxbet site');
-                            break;
-                        case 2:
-                            res = "Ladb";
-                            console.log('its Ladbrokes site');
-                            break;
-                        case 3:
-                            res = "Spor";
-                            console.log('its Sportsbet site');
-                            break;
-                        case 4:
-                            res = "will";
-                            console.log('its williamhill site');
-                            break;
-                        case 5:
-                            res = "unib";
-                            console.log('its unibet site');
-                            break;
-                        case 6:
-                            res = "Book";
-                            console.log('its Bookmaker site');
-                            break;
-                        case 7:
-                            res = "Crow";
-                            console.log('its Crownbet site');
+                    console.log(res);
+                    if (res === "ubet") {
+                        console.log('its ubet site');
                     }
-                    // if (res === 'ubet') {
-                    //     console.log('its ubet site');
-                    // }
-                    // else if (res === 'luxbet') {
-                    //     console.log('its luxbet site');
-                    // }
-                    // else if (res === 'luxbet') {
-                    //     console.log('its luxbet site');
-                    // }
+                    else if (res === "luxb") {
+                        console.log('its luxbet site');
+                    }
+                    else if (res === "Ladb") {
+                        console.log('its Ladbrokes site');
+                    }
+                    else if (res === "Spor") {
+                        console.log('its Sportsbet site');
+                    }
+                    else if (res === "will") {
+                        console.log('its williamhill site');
+                    }
+                    else if (res === "unib") {
+                        console.log('its unibet site');
+                    }
+                    else if (res === "Book") {
+                        console.log('its Bookmaker site');
+                    }
+                    else if (res === "Crow") {
+                        console.log('its Crownbet site');
+                    }
                 });
             }
 
@@ -143,39 +78,31 @@
                 getFixtureTwoBestHref().then(function (item) {
                     console.log(item);
                     var str = item;
-                    var res = str.substring(32, 38);
-                    switch (res) {
-                        case 0:
-                            res = "ubet";
-                            console.log('its ubet site');
-                            break;
-                        case 1:
-                            res = "luxb";
-                            console.log('its luxbet site');
-                            break;
-                        case 2:
-                            res = "Ladb";
-                            console.log('its Ladbrokes site');
-                            break;
-                        case 3:
-                            res = "Spor";
-                            console.log('its Sportsbet site');
-                            break;
-                        case 4:
-                            res = "will";
-                            console.log('its williamhill site');
-                            break;
-                        case 5:
-                            res = "unib";
-                            console.log('its unibet site');
-                            break;
-                        case 6:
-                            res = "Book";
-                            console.log('its Bookmaker site');
-                            break;
-                        case 7:
-                            res = "Crow";
-                            console.log('its Crownbet site');
+                    var res = str.substring(32, 36);
+                    console.log(res);
+                    if (res === "ubet") {
+                        console.log('its ubet site');
+                    }
+                    else if (res === "luxb") {
+                        console.log('its luxbet site');
+                    }
+                    else if (res === "Ladb") {
+                        console.log('its Ladbrokes site');
+                    }
+                    else if (res === "Spor") {
+                        console.log('its Sportsbet site');
+                    }
+                    else if (res === "will") {
+                        console.log('its williamhill site');
+                    }
+                    else if (res === "unib") {
+                        console.log('its unibet site');
+                    }
+                    else if (res === "Book") {
+                        console.log('its Bookmaker site');
+                    }
+                    else if (res === "Crow") {
+                        console.log('its Crownbet site');
                     }
                 });
             }
@@ -198,17 +125,6 @@
 
             test1();
             test2();
-
-            // console.log('fixture onr best href ' + test());
-
-
-            // console.log(fixtureOneBest.href);
-            // console.log(fixtureTwoBest.href);
-            // console.log(fixtureThreeBest.href);
-
-            // return console.log("fixture = " + fixture1Best + "" + fixtureTwo + "" + fixtureThree);
-
-            // if (fixture3)
         });
     };
 }).call(this);
